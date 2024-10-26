ca3fafa2d57e754db52611e02d2599e7e731e186
Mariana Ponce <atilanoponcemariana18@gmail.com>
2024-10-25 19:18:52 -0600

    AngularApi 25-10-24

---
+++ src/app/tem/temh/temh.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-temh',
+  standalone: true,
+  imports: [],
+  templateUrl: './temh.component.html',
+  styles: ``
+})
+export class TemhComponent {
+  @Input()mensaje:string='';
+}
